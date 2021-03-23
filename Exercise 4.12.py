47e081354eb4ad9795941a83edd17c05357fd463
Alya R <alyaramadhani000@gmail.com>
2021-03-23 17:32:42 +0700

    Exercise 4.12

--- Exercise 4.12.py
+++ Exercise 4.12.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 a = 60
 b = 13
 c = 0
@@ -5,8 +7,17 @@ c = 0
 c = a & b;
 print ("Line 1 - Value of c is", c)
 
-c = a  | b;
+c = a | b;
 print ("Line 2 - Value of c is", c)
 
 c = a ^ b;
 print ("Line 3 - Value of c is", c)
+
+c = ~a;
+print ("Line 4 - Value of c is", c)
+
+c = a << 2;
+print ("Line 5 - Value of c is", c)
+
+c = a >> 2;
+print ("Line 6 - Value of c is", c)
