666ca505507a99caf794959913f624dbfd75ef83
Alya R <alyaramadhani000@gmail.com>
2021-03-23 16:39:55 +0700

    Exercise 4.1

--- Exercise 4.1.py
+++ Exercise 4.1.py
@@ -3,3 +3,18 @@ y = 4
 
 # Output: x + y = 19
 print('x + y =', x + y)
+
+# Output: x - y = 11
+print('x - y =', x - y)
+
+# Output: x * y = 60
+print('x * y =', x * y)
+
+# Output: x / y = 13.75
+print('x / y =', x / y)
+
+# Output: x // y = 3
+print('x // y =', x // y)
+
+# Output: x ** y = 50625
+print('x ** y =', x ** y)
