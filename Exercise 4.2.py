d19926186149f656ccbdfbb399c9042cae264ab3
Alya R <alyaramadhani000@gmail.com>
2021-03-23 16:50:30 +0700

    Exercise 4.2

--- Exercise 4.2.py
+++ Exercise 4.2.py
@@ -1,2 +1,21 @@
+x = 15
 x = 10
 y = 12
+
+# Output: x > y is False
+print('x > y =', x > y)
+
+# Output: x < y is True
+print('x < y =', x < y)
+
+# Output: x == y is False
+print('x == y =', x == y)
+
+# Output: x != y is True
+print('x != y =', x != y)
+
+# Output: x >= y is False
+print('x >= y =', x >= y)
+
+# Output: x <= y is True
+print('x <= y =', x <= y)
