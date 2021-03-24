a400d6f5509d96764131b0ada55635be4af6037c
Alya R <alyaramadhani000@gmail.com>
2021-03-24 14:21:43 +0700

    soal 2

---
+++ I0320123_Soal 2.py
@@ -0,0 +1,5 @@
+# memasukkan bilangan bulat
+a = int(input("masukkan bilang bulat"))
+b = int(input("masukkan bilang bulat kedua"))
+
+print(a//b)
--- Soal 2.py
+++
@@ -1,5 +0,0 @@
-a = 22
-b = 10
-
-hasil = a // b
-print(a,'//',b,'=',hasil)
