eba6f6eddfad5adb3ce4fddc0674fbf7e24bffa6
Alya R <alyaramadhani000@gmail.com>
2021-03-24 15:15:35 +0700

    soal 4

---
+++ I0320123_Soal 4.py
@@ -0,0 +1,8 @@
+# syarat mendaftar kursus online
+umur = int(input("masukkan umur :"))
+ujian = input("apakah lulus ujian atau tidak :")
+
+if (umur>=21) and (ujian == "Y") :
+    print("Anda dapat mendaftar di kursus")
+else:
+    print("Anda tidka dapat mendaftar di kursus")
