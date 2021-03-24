d86a82f1ea263955c6003dd6c1cf8337ddfe2129
Alya R <alyaramadhani000@gmail.com>
2021-03-24 15:16:10 +0700

    soal 4

--- I0320123_Soal 4.py
+++ I0320123_Soal 4.py
@@ -5,4 +5,4 @@ ujian = input("apakah lulus ujian atau tidak :")
 if (umur>=21) and (ujian == "Y") :
     print("Anda dapat mendaftar di kursus")
 else:
-    print("Anda tidka dapat mendaftar di kursus")
+    print("Anda tidak dapat mendaftar di kursus")
