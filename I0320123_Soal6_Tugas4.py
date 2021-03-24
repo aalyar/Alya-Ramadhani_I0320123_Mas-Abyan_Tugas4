4549dafd2698eb82610bdb72b1de73b648c5ef11
Alya R <alyaramadhani000@gmail.com>
2021-03-24 16:42:39 +0700

    soal 6

--- I0320123_Soal6_Tugas4.py
+++ I0320123_Soal6_Tugas4.py
@@ -24,8 +24,7 @@ c = ~a
 print('nilai :',a,' , binary :',format(a,'08b'))
 print('nilai :',c,' , binary :',format(c,'08b'))
 
-#bitwise
-c = 11 & 4
+#bitwise AND (&)
 c = b & a
 print('nilai :',a,' , binary :',format(a,'08b'))
 print('nilai :',b,' , binary :',format(b,'08b'))
