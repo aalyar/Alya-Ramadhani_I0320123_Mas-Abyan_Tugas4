6cb9a2b90bada2e87904d3c822fecb19191d8443
Alya R <alyaramadhani000@gmail.com>
2021-03-24 16:32:48 +0700

    soal 6

---
+++ I0320123_Soal6_Tugas4.py
@@ -0,0 +1,32 @@
+a = 4
+b = 11
+
+# bitwise OR (|)
+c = a | b;
+print('nilai :',a,' , binary :',format(a,'08b'))
+print('nilai :',b,' , binary :',format(b,'08b'))
+print('nilai :',c,' , binary :',format(c,'08b'))
+
+# bitwise right shift (>>)
+c = a >> b;
+print('nilai :',a,' , binary :',format(a,'08b'))
+print('nilai :',b,' , binary :',format(b,'08b'))
+print('nilai :',c,' , binary :',format(c,'08b'))
+
+# bitwise XOR (^)
+c = a ^ b;
+print('nilai :',a,' , binary :',format(a,'08b'))
+print('nilai :',b,' , binary :',format(b,'08b'))
+print('nilai :',c,' , binary :',format(c,'08b'))
+
+# bitwise NOT (~)
+c = ~a
+print('nilai :',a,' , binary :',format(a,'08b'))
+print('nilai :',c,' , binary :',format(c,'08b'))
+
+#bitwise
+c = 11 & 4
+c = b & a
+print('nilai :',a,' , binary :',format(a,'08b'))
+print('nilai :',b,' , binary :',format(b,'08b'))
+print('nilai :',c,' , binary :',format(c,'08b'))
