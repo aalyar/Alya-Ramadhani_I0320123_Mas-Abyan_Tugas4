571f2f2f57bfdfaddbb7b4ea064094313e12c030
Alya R <alyaramadhani000@gmail.com>
2021-03-24 14:47:34 +0700

    soal 3

--- I0320123_Soal 3.py
+++ I0320123_Soal 3.py
@@ -1,11 +1,11 @@
 # input berat bagasi
-a = 110
+a = int(input("masukkan berat"))
 b = 49
 
 # membandingkan berat a
-hasil = a<50
+hasil = a<=50
 print(hasil)
 
 # membandingkan berat b
-hasil = b<50
+hasil = b<=50
 print(hasil)
